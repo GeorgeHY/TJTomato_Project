e8223f1456de1f43dda609c507ce743800e97d52
hanyang <hanyang@tjtomato.com>
2019-04-08 12:03:29 +0800

    首次提交

---
+++ FQProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FQProject
+//
+//  Created by 韩扬 on 2019/4/8.
+//  Copyright © 2019 韩扬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FQProjectTests/FQProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  FQProjectTests.swift
+//  FQProjectTests
+//
+//  Created by 韩扬 on 2019/4/8.
+//  Copyright © 2019 韩扬. All rights reserved.
+//
+
+import XCTest
+@testable import FQProject
+
+class FQProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
